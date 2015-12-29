383ffe90dcc3b2024acda538cfff0bbeafa2e162
Alan Joyce <alan@thisisalan.com>
2015-12-29 23:25:48 +0000

    updating layout

--- js/dash.js
+++ js/dash.js
@@ -1,1 +1,13 @@
- 
+function setUA(window, UA) {
+  if(window.navigator.userAgent != UA) {
+    var userAgentProp = {get: function() {return UA;}};
+    try {
+      Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+    } catch(e) {
+      window.navigator = Object.create(navigator, {
+        userAgent: userAgentProp
+      });
+    }
+  }
+}
+
