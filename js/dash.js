057da64172417df2ef4440faa64cd1252be8219c
Alan Joyce <alan@thisisalan.com>
2015-12-30 00:48:27 +0000

    JS updates

--- js/dash.js
+++ js/dash.js
@@ -1,13 +1,9 @@
-function setUA(window, UA) {
-  if(window.navigator.userAgent != UA) {
-    var userAgentProp = {get: function() {return UA;}};
-    try {
-      Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
-    } catch(e) {
-      window.navigator = Object.create(navigator, {
-        userAgent: userAgentProp
-      });
-    }
-  }
+var currentPosition;
+
+function init() {
+  navigator.geolocation.getCurrentPosition(updatePosition);
 }
 
+function updatePosition(position) {
+  currentPosition = position;
+}
